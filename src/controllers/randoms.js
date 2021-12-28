2d12470ca6da7079f66acd3b7a1c1cb1276cee90
Demian <demianpoggio@gmail.com>
2021-12-28 20:19:56 -0300

    Se convierte el servidor en no bloqueante

--- src/controllers/randoms.js
+++ src/controllers/randoms.js
@@ -1,16 +1,13 @@
 const cfg = require('../config.js')
 
-class RandomController {
-    constructor(){}
-
-    getRandomNumber(maxValue = cfg.RANDOM_MAX_VALUE){
+const getRandomNumber = (maxValue = cfg.RANDOM_MAX_VALUE) => {
         return Math.floor(Math.random()*maxValue)+1
     }
 
-    getRandoms(cant = cfg.RANDOM_DEFAULT_CANT){
+const getRandoms = (cant = cfg.RANDOM_DEFAULT_CANT) => {
         const numbers = {}
         for (let i = 0; i<cant; i++){
-            const number = this.getRandomNumber()
+            const number = getRandomNumber()
             if (!numbers[number]) {
                 numbers[number] = 1
             } else {
@@ -19,6 +16,8 @@ class RandomController {
         }
         return numbers
     }
-}
 
-module.exports = RandomController;
+process.on('message', msg => {
+    const listaRandom = getRandoms(msg.cant)
+    process.send(listaRandom)
+})
--- src/routers/routerRandoms.js
+++ src/routers/routerRandoms.js
@@ -1,15 +1,18 @@
 const { Router } = require('express')
-const RandomController = require('../controllers/randoms.js')
+const { fork } = require('child_process')
 
 const routerRandoms = Router();
-const randomController = new RandomController()
 
 /**** Rutas ****/
 routerRandoms.get('/', (req, res, next) => {  
     try {
         const cant = req.query.cant
-        const listaRandom = randomController.getRandoms(cant)
-        res.json(listaRandom)    
+
+        const randomController = fork('./src/controllers/randoms.js')
+        randomController.send({ cant })
+        randomController.on('message', listaRandom => {
+            res.json(listaRandom)
+        })
     } catch (error) {
         next(error)
     }
